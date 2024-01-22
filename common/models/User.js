8bb778d0b5d7dd4f46e949c7ee69c1de5aea4247
PythonXHunter <aryan.computerscience@gmail.com>
2024-01-22 15:53:22 +0530

    create user model

---
+++ common/models/User.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const { roles } = require("../../config");
+
+const UserModel = {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull : false,
+    unique: true,
+  }
+}
